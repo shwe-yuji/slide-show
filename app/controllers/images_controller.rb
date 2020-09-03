d128955e721e7c716e875c8a0c48e48bf02ce3ae
shwe-yuji <okumura19943210yuji@gmail.com>
2020-09-03 20:33:07 +0900

    削除機能

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -6,6 +6,12 @@ class ImagesController < ApplicationController
     redirect_to root_path
   end
 
+  def destroy
+    image = Image.find(params[:id])
+    image.destroy
+    redirect_to root_path
+  end
+
   private
 
   def image_params
