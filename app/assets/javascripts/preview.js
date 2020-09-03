b91070fc85de10f2c1208d77a39d83fdd5540394
shwe-yuji <okumura19943210yuji@gmail.com>
2020-09-03 11:49:57 +0900

    サイズ変更

--- app/assets/javascripts/preview.js
+++ app/assets/javascripts/preview.js
@@ -3,7 +3,7 @@ $(document).on('turbolinks:load', function() {
   function buildHTML(image){
     var html = 
       `
-        <img src="${image}", class="image-upload__form__preview--image">
+        <img src="${image}", class="image-upload__form__preview--image", width="150", height="100">
       `
     return html;
   };
